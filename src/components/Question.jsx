8299fccb1c4ade3fd2b93e83cd4788f31e70c1de
SirSnehGupta <guptasneh302004@gmail.com>
2023-01-21 19:55:52 +0530

    added component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-
+import he from 'he';
+import Choice from "./Choice";
 export default function Question(props){
+    const [options, setOptions]=React.useState([{isCorrect: false, value:props.incorrect[0]},
+                    {isCorrect: false, value:props.incorrect[1]},
+                    {isCorrect: false, value:props.incorrect[2]},
+                    {isCorrect: true, value:props.correct}])
+    React.useEffect(()=> {setOptions(prevOptions => prevOptions.sort(()=>Math.random()-0.5))
+                            console.log("effect ran")}, [])
+                    // const options = [...props.incorrect, props.correct]
+    const choices = options.map(opt =>(
+        <Choice key={options.indexOf(opt)} submit={props.submit} ques={props.question} value={he.decode(opt.value)} isCorrect={opt.isCorrect} />
+    ))
+
     return(
+        <div className="quiz-ui">
         <section className="question">
-            <h2>Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?</h2>
-            <span className="option">Cabbage Patch Kids</span>
-            <span className="option">Transformers</span>
-            <span className="option">Carebears</span>
-            <span className="option">Rubik's Cube</span>
+            <h2>{he.decode(props.question)}</h2>
+            {choices}
         </section>
+        <hr></hr>
+        </div>
     )
 }
